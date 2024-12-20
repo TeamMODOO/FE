e8ca428edd67532d4a4c544a29b6ee9dae36d3d5
Han-Seung-Chan <seungchanhan00@gmail.com>
2024-12-20 20:02:12 +0900

    ✨ Feat: 사이드바 구현

---
+++ src/components/sideBar/homeSideBar.tsx
@@ -0,0 +1,49 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export const HomeSideBar = () => {
+  return (
+    <Sidebar>
+      <Tabs defaultValue="account" className="w-full">
+        <SidebarHeader>
+          <TabsList>
+            <TabsTrigger value="account">내 정보</TabsTrigger>
+            <TabsTrigger value="password">친구 정보</TabsTrigger>
+            <TabsTrigger value="settings">전체 채팅</TabsTrigger>
+          </TabsList>
+        </SidebarHeader>
+
+        <SidebarContent>
+          <SidebarContent>
+            <TabsContent value="account">
+              <div className="p-4">
+                <h3 className="text-lg font-medium">내 정보</h3>
+                <p>위 글 지우고 내 정보 관련 컴포넌트가 들어가면 됨</p>
+              </div>
+            </TabsContent>
+          </SidebarContent>
+        </SidebarContent>
+        <SidebarContent>
+          <TabsContent value="password">
+            <div className="p-4">
+              <h3 className="text-lg font-medium">친구 정보</h3>
+              <p>위 글 지우고 친구 정보 관련 컴포넌트가 들어가면 됨</p>
+            </div>
+          </TabsContent>
+        </SidebarContent>
+        <SidebarContent>
+          <TabsContent value="settings">
+            <div className="p-4">
+              <h3 className="text-lg font-medium">전체 채팅</h3>
+              <p>위 글 지우고 전체 채팅 관련 컴포넌트가 들어가면 됨</p>
+            </div>
+          </TabsContent>
+        </SidebarContent>
+      </Tabs>
+    </Sidebar>
+  );
+};
