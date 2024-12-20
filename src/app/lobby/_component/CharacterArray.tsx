449cb3898030615fe0e83a1c465465c268038408
김희원 <julian3306@naver.com>
2024-12-20 22:13:27 +0900

    ✨ Feat: 캐릭터 움직임 구현

--- src/app/lobby/_component/CharacterArray.tsx
+++ src/app/lobby/_component/CharacterArray.tsx
@@ -1,4 +1,4 @@
-const characterImages = {
+const characterImages: Record<"character1" | "character2", string> = {
   character1: "/character/character1.png",
   character2: "/character/character2.png",
 };
