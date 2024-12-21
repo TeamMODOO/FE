e2837034e41a1a4f7edf9160070e9d76190b27ad
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-21 19:33:08 +0900

    🔨 Refactor: 불필요한 Provider 제거

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -1,13 +1,7 @@
 import { ReactNode } from "react";
 
-import { SidebarProvider } from "@/components/ui/sidebar";
-
 import { QueryProvider } from "./query-provider";
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
-  return (
-    <QueryProvider>
-      <SidebarProvider>{children} </SidebarProvider>
-    </QueryProvider>
-  );
+  return <QueryProvider>{children}</QueryProvider>;
 };
