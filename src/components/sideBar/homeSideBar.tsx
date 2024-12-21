36ebb63a4a595f25941cb489a3b3036eebc0b3b5
김희원 <julian3306@naver.com>
2024-12-21 21:32:41 +0900

    ✨ Feat: 사이드바 친구 정보 컴포넌트 구현

--- src/components/sideBar/homeSideBar.tsx
+++ src/components/sideBar/homeSideBar.tsx
@@ -5,10 +5,14 @@ import {
 } from "@/components/ui/sidebar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+import { FriendInformation } from "./FriendInformation";
+
 export const HomeSideBar = () => {
   return (
-    <Sidebar>
-      <Tabs defaultValue="account" className="w-full">
+    <Sidebar className="h-screen">
+      {" "}
+      {/* 전체 화면 높이 설정 */}
+      <Tabs defaultValue="account" className="size-full">
         <SidebarHeader>
           <TabsList>
             <TabsTrigger value="account">내 정보</TabsTrigger>
@@ -17,25 +21,17 @@ export const HomeSideBar = () => {
           </TabsList>
         </SidebarHeader>
 
-        <SidebarContent>
-          <SidebarContent>
-            <TabsContent value="account">
-              <div className="p-4">
-                <h3 className="text-lg font-medium">내 정보</h3>
-                <p>위 글 지우고 내 정보 관련 컴포넌트가 들어가면 됨</p>
-              </div>
-            </TabsContent>
-          </SidebarContent>
-        </SidebarContent>
-        <SidebarContent>
-          <TabsContent value="password">
+        <SidebarContent className="h-full overflow-y-auto">
+          {/* 스크롤 가능 */}
+          <TabsContent value="account">
             <div className="p-4">
-              <h3 className="text-lg font-medium">친구 정보</h3>
-              <p>위 글 지우고 친구 정보 관련 컴포넌트가 들어가면 됨</p>
+              <h3 className="text-lg font-medium">내 정보</h3>
+              <p>위 글 지우고 내 정보 관련 컴포넌트가 들어가면 됨</p>
             </div>
           </TabsContent>
-        </SidebarContent>
-        <SidebarContent>
+          <TabsContent value="password">
+            <FriendInformation />
+          </TabsContent>
           <TabsContent value="settings">
             <div className="p-4">
               <h3 className="text-lg font-medium">전체 채팅</h3>
