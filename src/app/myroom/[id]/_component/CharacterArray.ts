69bea402a7720fa8dc7c82ec073911e64d8eab35
김희원 <julian3306@naver.com>
2024-12-23 21:10:04 +0900

    ✨ Feat: 버튼 클릭시 마이룸에 gif 업데이트 로직 추가

--- src/app/myroom/[id]/_component/CharacterArray.ts
+++ src/app/myroom/[id]/_component/CharacterArray.ts
@@ -1,12 +1,6 @@
-const interiorImages = {
-  none: "/interior/none.gif",
-  interior1: "/interior/interior1.gif",
-  interior2: "/interior/interior2.gif",
-  interior3: "/interior/interior3.gif",
-  interior4: "/interior/interior4.gif",
-  interior5: "/interior/interior5.gif",
-  interior6: "/interior/interior6.gif",
-  interior7: "/interior/interior7.gif",
+const characterImages = {
+  character1: "/character/character1.png",
+  character2: "/character/character2.png",
 };
 
-export default interiorImages;
+export default characterImages;
---
+++ src/app/myroom/[id]/_model/Funiture.ts
@@ -0,0 +1,7 @@
+export interface Funiture {
+  id: string;
+  x: number;
+  y: number;
+  funitureType: string; // 더 이상 리터럴 타입이 아님
+  funiturename: string;
+}
