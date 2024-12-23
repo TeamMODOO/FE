6593c6286768663ba9bdd88bd729bd08024b787c
김희원 <julian3306@naver.com>
2024-12-23 19:19:36 +0900

    💄 Design: 마이룸 가구 더미 이미지 등록

---
+++ src/app/myroom/[id]/_component/CharacterArray.ts
@@ -0,0 +1,12 @@
+const interiorImages = {
+  none: "/interior/none.gif",
+  interior1: "/interior/interior1.gif",
+  interior2: "/interior/interior2.gif",
+  interior3: "/interior/interior3.gif",
+  interior4: "/interior/interior4.gif",
+  interior5: "/interior/interior5.gif",
+  interior6: "/interior/interior6.gif",
+  interior7: "/interior/interior7.gif",
+};
+
+export default interiorImages;
