33e6ad8331fccb248ddab585ee2203b38ac539b6
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-23 18:48:21 +0900

    📦️ Chore: 로그인 완료 시 캐릭터 생성 페이지로 리디렉션

--- src/auth.ts
+++ src/auth.ts
@@ -2,10 +2,17 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  debug: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      // 기본값으로 /registercharacter 설정
+      return `${baseUrl}/registercharacter`;
+    },
+  },
 });
