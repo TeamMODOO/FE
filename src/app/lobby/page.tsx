c6ca96d49084391799802ac0a71501681a074a41
Han-Seung-Chan <seungchanhan00@gmail.com>
2024-12-24 23:11:04 +0900

    🔨 Refactor: useMainSocketConnect 호출 위치 변경

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -1,11 +1,5 @@
-"use client";
-
-import useMainSocketConnect from "@/hooks/useMainSocketConnect";
-
 import LobbyCanvas from "./_component/Canvas/Canvas";
 
 export default function Page() {
-  useMainSocketConnect();
-
   return <LobbyCanvas></LobbyCanvas>;
 }
