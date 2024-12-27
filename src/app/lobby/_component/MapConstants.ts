d4afd06bfecbb2da029f13d17080da72002cda54
김희원 <julian3306@naver.com>
2024-12-28 01:27:21 +0900

    ✨ Feat: 마이룸에서 캐릭터 이동 구현

--- src/app/lobby/_component/MapConstants.ts
+++ src/app/lobby/_component/MapConstants.ts
@@ -1,11 +1,11 @@
 export const MAP_CONSTANTS = {
-  CANVAS_WIDTH: 800,
-  CANVAS_HEIGHT: 600,
+  CANVAS_WIDTH: 1400,
+  CANVAS_HEIGHT: 800,
   IMG_WIDTH: 50,
   IMG_HEIGHT: 150,
   SPEED: 6,
 
   // 새로 추가 (맵 전체 크기)
-  MAP_WIDTH: 800, // 가로 2000px 맵
-  MAP_HEIGHT: 600, // 세로 1500px 맵
+  MAP_WIDTH: 1200, // 가로 2000px 맵
+  MAP_HEIGHT: 700, // 세로 1500px 맵
 };
