f0cd4b9991d3d3d9af6a453ec3b3f15d6ac94fc2
김희원 <julian3306@naver.com>
2024-12-28 01:27:21 +0900

    ✨ Feat: 마이룸에서 캐릭터 이동 구현

--- src/app/myroom/[id]/_model/User.ts
+++ src/app/myroom/[id]/_model/User.ts
@@ -4,4 +4,9 @@ export interface User {
   y: number;
   characterType: string;
   nickname: string;
+
+  direction?: Direction; // 0=Down,1=Up,2=Right,3=Left
+  isMoving?: boolean;
 }
+
+export type Direction = 0 | 1 | 2 | 3;
