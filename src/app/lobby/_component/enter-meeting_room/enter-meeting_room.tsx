34b2f499c359fdd1a7fbfb537d489f9d60b32630
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-30 19:37:46 +0900

    🔨 Refactor: 사이드바 삭제 후 발생하는 오류 해결

--- src/app/lobby/_component/enter-meeting_room/enter-meeting_room.tsx
+++ src/app/lobby/_component/enter-meeting_room/enter-meeting_room.tsx
@@ -12,6 +12,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import { RoomCard } from "./room";
 
 /**
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-"use client";
-
-import { EnterMeetingRoom } from "@/components";
-import useMainSocketConnect from "@/hooks/socket/useMainSocketConnect";
-
 export default function Home() {
-  useMainSocketConnect();
   return (
     <div className="mx-auto grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       메인 페이지
-      <EnterMeetingRoom />
     </div>
   );
 }
