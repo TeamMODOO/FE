849eb416107174f5a7a09d63115351ab56c4afea
Han-Seung-Chan <seungchanhan00@gmail.com>
2024-12-31 00:24:02 +0900

    🔨 Refactor: 파일 타입 변경

--- src/app/api/portfolio/route.ts
+++ src/app/api/portfolio/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
   try {
     // 1) formData로 파일 받기
     const formData = await request.formData();
-    const file = formData.get("file") as Blob | null;
+    const file = formData.get("file") as File | null;
 
     if (!file) {
       return NextResponse.json({ error: "No file provided" }, { status: 400 });
