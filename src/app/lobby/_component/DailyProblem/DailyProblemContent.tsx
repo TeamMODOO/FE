f5ea36bce5ca626f9beacc7c0066684de3023ed2
김희원 <julian3306@naver.com>
2024-12-30 23:01:09 +0900

    💄 Design: 메인 로비 백준 NPC 대사 수정

--- src/app/lobby/_component/DailyProblem/DailyProblemContent.tsx
+++ src/app/lobby/_component/DailyProblem/DailyProblemContent.tsx
@@ -11,24 +11,45 @@ import Style from "./DailyProblemContent.style";
  */
 const DailyProblemContent: React.FC = () => {
   const router = useRouter();
+  const content = `“오, 거기 지나가는 용사여!
+정글 타워 옥상에 용이 나타나 정글러들을 위협하고 있다네.
+자네의 도움이 절실하니, 당장 용을 처단하고 이 땅의 평화를 되찾아주겠나?"`;
 
   return (
     <div className={Style.container}>
-      <h3 className={Style.heading}>백준 문제를 풀러 가시겠습니까?</h3>
-      <p className="my-4">이동 버튼을 누르면 /quest 페이지로 이동합니다.</p>
+      <h3 className={Style.heading}>정글의 수석코치</h3>
+      <p className="my-4 whitespace-pre-line">{content}</p>
 
-      <button
-        onClick={() => router.push("/quest")}
-        className="rounded bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700"
-      >
-        이동하기
-      </button>
-      <button
-        onClick={() => router.push("/questmap")}
-        className="rounded bg-blue-600 px-4 py-2 font-bold text-white hover:bg-blue-700"
-      >
-        이동하기22
-      </button>
+      {/* 버튼을 세로로 배치 (flex-col) */}
+      <div className="flex flex-col gap-2">
+        <button
+          onClick={() => router.push("/quest")}
+          className="
+            cursor-pointer 
+            py-2 
+            text-left 
+            text-blue-600 
+            hover:text-blue-800 
+            hover:underline
+          "
+        >
+          [일일 퀘스트] 백준 문제를 풀어 용을 처치하자!
+        </button>
+
+        <button
+          onClick={() => router.push("/questmap")}
+          className="
+            cursor-pointer 
+            py-2 
+            text-left 
+            text-blue-600 
+            hover:text-blue-800 
+            hover:underline
+          "
+        >
+          명예의 전당으로 바로 이동…
+        </button>
+      </div>
     </div>
   );
 };
