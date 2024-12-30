b06d3b9172f2fdc9f47023819e3833bb038a2418
Han-Seung-Chan <seungchanhan00@gmail.com>
2024-12-30 15:44:08 +0900

    ✨ Feat: 채팅 및 유저목록 사이드바에서 위젯으로 변경

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -3,8 +3,16 @@
 import useMainSocketConnect from "@/hooks/socket/useMainSocketConnect";
 
 import LobbyCanvas from "./_component/Canvas/Canvas";
+import ChatWidget from "./_component/Widget/Chatting";
+import FriendInformation from "./_component/Widget/FriendInformation";
 
 export default function Page() {
   useMainSocketConnect();
-  return <LobbyCanvas></LobbyCanvas>;
+  return (
+    <>
+      <LobbyCanvas></LobbyCanvas>
+      <ChatWidget />
+      <FriendInformation />
+    </>
+  );
 }
