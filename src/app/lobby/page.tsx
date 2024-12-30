8b7afbffac98f672c4a76c9bf43322d3f50cf204
Choi Min Kyu <chlalsrb118@gmail.com>
2024-12-30 16:18:18 +0900

    ✨ Feat: Google Login API 연결부 작성

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useSignIn } from "@/hooks/signin/useSignIn";
 import useMainSocketConnect from "@/hooks/socket/useMainSocketConnect";
 
 import LobbyCanvas from "./_component/Canvas/Canvas";
@@ -7,6 +8,8 @@ import ChatWidget from "./_component/Widget/Chatting";
 import FriendInformation from "./_component/Widget/FriendInformation";
 
 export default function Page() {
+  const { session, status, loginMessage } = useSignIn();
+
   useMainSocketConnect();
   return (
     <>
