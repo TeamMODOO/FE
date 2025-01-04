8b0ed4c164205f2b3bceb86f712d46bf1cb18fc5
Choi Min Kyu <chlalsrb118@gmail.com>
2025-01-04 21:04:09 +0900

    🚑️ Fix: 로그인 시 API 요청 과다 문제 픽스

--- src/app/signinloading/page.tsx
+++ src/app/signinloading/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import { useSignInPost } from "@/hooks/signin/useSignInPost";
 
@@ -20,27 +20,24 @@ export default function SignInLoading() {
   const { data: session, status } = useSession();
   const { signIn, loading } = useSignInPost(); // custom hook
   const router = useRouter();
+  // api 요청을 한 번만 보내기 위한 flag
+  const didSignInRef = useRef(false);
 
   useEffect(() => {
-    // 1) status === "authenticated" 이면 구글 로그인 상태임
-    if (status === "authenticated") {
+    // 1) 구글 로그인 상태이면서, api 요청 전
+    if (status === "authenticated" && !didSignInRef.current) {
       const hasToken = hasAccessTokenCookie();
 
       if (hasToken) {
-        // 2) access_token 쿠키가 존재하면 바로 /lobby로 이동
+        // 2) 구글 로그인 상태이면서, api 요청 전이고, access_token 쿠키가 존재
         router.push("/lobby");
       } else {
-        // 3) 구글 로그인이 되었지만 access_token이 없으면 signIn() 호출
+        didSignInRef.current = true;
+        // 3) 구글 로그인 상태이면서, api 요청 전이고, access_token이 없으면 signIn() 호출
         // -> 토큰 발급 후 로비로 이동
         signIn()
           .then(() => {
             router.push("/lobby");
-            // const updatedHasToken = hasAccessTokenCookie();
-            // if (updatedHasToken) {
-            //   router.push("/lobby");
-            // } else {
-            //   alert("에러가 발생했습니다 : access_token이 발급되지 않습니다.");
-            // }
           })
           .catch((err) => {
             // 에러 처리 로직을 여기에 추가해도 됨
