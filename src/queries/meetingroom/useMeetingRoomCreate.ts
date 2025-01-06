0e60d5571939a3d1a2c9b01ff469590990d44df9
Han-Seung-Chan <seungchanhan00@gmail.com>
2025-01-07 00:20:50 +0900

    🔨 Refactor: 미팅룸 타입 분리

--- src/queries/meetingroom/useMeetingRoomCreate.ts
+++ src/queries/meetingroom/useMeetingRoomCreate.ts
@@ -3,27 +3,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
-// 미팅룸 타입 정의
-interface MeetingRoom {
-  id: string;
-  title: string;
-}
+import { CreateMeetingRoomPayload } from "@/model/MeetingRoom";
 
-// 미팅룸 생성 시 필요한 입력 타입
-export interface CreateMeetingRoomPayload {
-  room_id: string;
-  title: string;
-  client_id: string;
-}
-
-// 미팅룸 생성 훅
 export function useCreateMeetingRoom() {
   const queryClient = useQueryClient();
 
   async function createMeetingRoom(
     payload: CreateMeetingRoomPayload,
-  ): Promise<MeetingRoom> {
-    const { data } = await axios.post<MeetingRoom>(
+  ): Promise<void> {
+    await axios.post(
       `${process.env.NEXT_PUBLIC_API_SERVER_PATH}/meetingroom/create`,
       payload,
       {
@@ -33,10 +21,9 @@ export function useCreateMeetingRoom() {
         },
       },
     );
-    return data;
   }
 
-  return useMutation<MeetingRoom, Error, CreateMeetingRoomPayload>({
+  return useMutation<void, Error, CreateMeetingRoomPayload>({
     mutationFn: async (payload) => await createMeetingRoom(payload),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["meetingroom"] });
