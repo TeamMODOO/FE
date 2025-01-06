b976b9d9f30ce5f12671ef8c11f3017095be85f7
Han-Seung-Chan <seungchanhan00@gmail.com>
2025-01-07 00:20:50 +0900

    🔨 Refactor: 미팅룸 타입 분리

--- src/queries/meetingroom/useMeetingRoom.ts
+++ src/queries/meetingroom/useMeetingRoom.ts
@@ -3,17 +3,14 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+import { MeetingRoom } from "@/model/MeetingRoom";
+
 // 미팅룸 타입 정의
-interface MeetingRoom {
-  room_id: string;
-  title: string;
-  clients: string[];
-}
 
 // 미팅룸 조회 훅
 export function useMeetingRoom() {
-  const fetchMeetingRoom = async (): Promise<MeetingRoom> => {
-    const { data } = await axios.get<MeetingRoom>(
+  const fetchMeetingRoom = async (): Promise<MeetingRoom[]> => {
+    const { data } = await axios.get<MeetingRoom[]>(
       `${process.env.NEXT_PUBLIC_API_SERVER_PATH}/meetingroom/list`,
     );
     return data;
