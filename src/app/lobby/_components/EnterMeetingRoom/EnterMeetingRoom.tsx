794827803e45d27c6d19b4d24bea12397279cd2c
Choi Min Kyu <chlalsrb118@gmail.com>
2025-01-08 00:52:02 +0900

    ✨ Feat: 게스트 로그인 시 회의실 방 생성 불가

--- src/app/lobby/_components/EnterMeetingRoom/EnterMeetingRoom.tsx
+++ src/app/lobby/_components/EnterMeetingRoom/EnterMeetingRoom.tsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 
 import { useRouter } from "next/navigation";
 
+import { useSession } from "next-auth/react";
+
 import { RefreshCw } from "lucide-react";
 import { v4 as uuid } from "uuid";
 
@@ -32,6 +34,8 @@ export const EnterMeetingRoom: React.FC<EnterMeetingRoomProps> = ({
   open,
   onOpenChange,
 }) => {
+  const { data: session } = useSession();
+
   const [roomName, setRoomName] = useState("");
   // 데이터 요청
   const { data, isLoading, isError, refetch } = useMeetingRoom();
@@ -95,21 +99,39 @@ export const EnterMeetingRoom: React.FC<EnterMeetingRoomProps> = ({
           </TabsContent>
 
           {/* 방 만들기 탭 */}
-          <TabsContent value="make">
-            <div className="flex flex-col gap-4 py-4">
-              <div className="flex flex-col gap-2">
-                <Label htmlFor="roomName">방 이름</Label>
-                <Input
-                  id="roomName"
-                  value={roomName}
-                  onChange={(e) => setRoomName(e.target.value)}
-                  placeholder="방 이름을 입력하세요"
-                />
+          <TabsContent value="make" className="h-full">
+            {session?.user?.role === "guest" ? (
+              <div className="flex h-full flex-col justify-between">
+                <div className="mt-20 text-center">
+                  <p>게스트 로그인 상태입니다.</p>
+                  <p>방 참여만 가능합니다.</p>
+                </div>
+                <Button
+                  type="submit"
+                  className="text-center"
+                  onClick={() => {
+                    router.push("/signin");
+                  }}
+                >
+                  로그인 하러가기
+                </Button>
               </div>
-              <Button type="submit" className="mt-4" onClick={handleSubmit}>
-                생성하기
-              </Button>
-            </div>
+            ) : (
+              <div className="flex flex-col gap-4 py-4">
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="roomName">방 이름</Label>
+                  <Input
+                    id="roomName"
+                    value={roomName}
+                    onChange={(e) => setRoomName(e.target.value)}
+                    placeholder="방 이름을 입력하세요"
+                  />
+                </div>
+                <Button type="submit" className="mt-4" onClick={handleSubmit}>
+                  생성하기
+                </Button>
+              </div>
+            )}
           </TabsContent>
         </Tabs>
       </DialogContent>
