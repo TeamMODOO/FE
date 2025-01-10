04ac45dbd76c952c6fbc9a029a0bef50f9853a28
Han-Seung-Chan <seungchanhan00@gmail.com>
2025-01-10 16:06:15 +0900

    🔨 Refactor: zustand socket 타입 수정

--- src/model/socket.ts
+++ src/model/socket.ts
@@ -1,13 +1,5 @@
 import { Socket } from "socket.io-client";
 
-export interface SocketStoreType {
-  socket: Socket | null;
-  isConnected: boolean;
-  setSocket: (socket: Socket | null) => void;
-  setIsConnected: (isConnected: boolean) => void;
-}
-
-// 수정된 타입(임시)
 export interface SocketState {
   socket: Socket | null;
   isConnected: boolean;
