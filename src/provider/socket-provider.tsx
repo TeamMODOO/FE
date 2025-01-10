3ae507835d7e0c8ba34bd60118c160be7adcbc78
Han-Seung-Chan <seungchanhan00@gmail.com>
2025-01-10 18:57:40 +0900

    🔨 Refactor: 새로고침 했을 때 소켓 연결 끊기

--- src/provider/socket-provider.tsx
+++ src/provider/socket-provider.tsx
@@ -12,19 +12,30 @@ import useSocketStore from "@/store/useSocketStore";
 export function SocketProvider({ children }: PropsWithChildren) {
   const { data: session, status } = useSession();
   const { setSocket, setIsConnected, reset } = useSocketStore();
-
   const { clientId, initializeClientId } = useClientIdStore();
 
-  // 세션 상태가 변경될 때 클라이언트 ID 초기화
   useEffect(() => {
     if (status === "loading") return;
-    if (session && session.user && (session.user.id || session.user.guest_id))
+    if (session?.user?.id || session?.user?.guest_id) {
       initializeClientId(session.user.id);
+    }
   }, [session]);
 
   useEffect(() => {
-    // 세션 로딩 중이면 소켓 연결하지 않음
     if (status === "loading" || !clientId) return;
+
+    // 기존 소켓 연결을 정리
+    const cleanup = () => {
+      if (socket) {
+        socket.emit("CS_USER_DESTRUCTION", {
+          client_id: clientId,
+        });
+        socket.disconnect();
+      }
+    };
+
+    // 페이지를 떠날 때 동기적으로 처리하기 위한 함수
+    const handleBeforeUnload = () => cleanup();
 
     // 소켓 인스턴스 생성
     const socket = io(process.env.NEXT_PUBLIC_BASE_URL!, {
@@ -38,29 +49,26 @@ export function SocketProvider({ children }: PropsWithChildren) {
       },
     });
 
-    // 소켓 이벤트 핸들러 등록
     socket.on("connect", () => {
+      setSocket(socket);
       setIsConnected(true);
+      window.addEventListener("beforeunload", handleBeforeUnload);
     });
 
-    socket.on("disconnect", (reason) => {
+    socket.on("disconnect", () => {
       setIsConnected(false);
+
+      window.removeEventListener("beforeunload", handleBeforeUnload);
     });
 
     socket.on("connect_error", (error) => {
       setIsConnected(false);
     });
 
-    // 소켓 인스턴스를 스토어에 저장
-    setSocket(socket);
-
-    // 컴포넌트 언마운트 시 정리
+    // 컴포넌트 언마운트나 clientId 변경 시 정리
     return () => {
-      socket.off("connect");
-      socket.off("disconnect");
-      socket.off("connect_error");
-      socket.off("reconnect");
-      socket.disconnect();
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      cleanup();
       reset();
     };
   }, [clientId]);
