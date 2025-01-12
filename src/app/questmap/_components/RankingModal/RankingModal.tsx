c1c995958965259a8e2fb665d3ab0bcbe61af4fb
Choi Min Kyu <chlalsrb118@gmail.com>
2025-01-12 16:49:54 +0900

    💄 Design: 랭킹 모달 디자인 수정

--- src/app/questmap/_components/RankingModal/RankingModal.tsx
+++ src/app/questmap/_components/RankingModal/RankingModal.tsx
@@ -58,13 +58,50 @@ export default function RankingModal({ onClose }: RankingModalProps) {
 
           {/* 4. 데이터가 있을 때 표시 */}
           {data &&
-            data.map((result, idx) => (
-              <div key={result.id} className={styles.rankingItem}>
-                <p>{idx + 1}</p> {/* 랭킹(순위) */}
-                <p>{result.user_name}</p>
-                <p>{result.time_taken}</p>
-              </div>
-            ))}
+            data.map((result, idx) => {
+              let extraClass = "";
+              if (idx === 0) {
+                extraClass = styles.rankingItemGold;
+              } else if (idx === 1) {
+                extraClass = styles.rankingItemSilver;
+              } else if (idx === 2) {
+                extraClass = styles.rankingItemBronze;
+              }
+
+              return (
+                <div
+                  key={result.id}
+                  className={`${styles.rankingItem} ${extraClass}`}
+                >
+                  <p className="flex">
+                    {idx === 0 && (
+                      <img
+                        src="/npc_event/firstmedal.png"
+                        alt="금메달"
+                        style={{ width: "1.5rem", marginRight: "0.5rem" }}
+                      />
+                    )}
+                    {idx === 1 && (
+                      <img
+                        src="/npc_event/secondmedal.png"
+                        alt="은메달"
+                        style={{ width: "1.5rem", marginRight: "0.5rem" }}
+                      />
+                    )}
+                    {idx === 2 && (
+                      <img
+                        src="/npc_event/thirdmedal.png"
+                        alt="동메달"
+                        style={{ width: "1.5rem", marginRight: "0.5rem" }}
+                      />
+                    )}
+                    {idx + 1}
+                  </p>
+                  <p>{result.user_name}</p>
+                  <p>{result.time_taken}</p>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
