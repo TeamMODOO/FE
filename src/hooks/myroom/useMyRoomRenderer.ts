3e6b97025dea2acbfe099bda53f883ff108667a8
김희원 <julian3306@naver.com>
2025-01-16 13:16:00 +0900

    💄 Design: 마이룸 이미지 리소스 변경

--- src/hooks/myroom/useMyRoomRenderer.ts
+++ src/hooks/myroom/useMyRoomRenderer.ts
@@ -10,6 +10,7 @@ import {
 } from "@/hooks/performance/useLoadSprites";
 import { Funiture } from "@/model/Funiture";
 import { User } from "@/model/User";
+import { MYROOM_COLLISION_ZONES } from "@/app/myroom/[google_id]/_constant";
 
 const MAP_WIDTH = 2000;
 const MAP_HEIGHT = 900;
@@ -334,6 +335,15 @@ export function useMyRoomRenderer({
       ctx.fillStyle = "blue";
       ctx.fillRect(x, y, 60, 120);
     }
+
+    // 충돌 영역
+    MYROOM_COLLISION_ZONES.forEach((zone) => {
+      ctx.fillStyle = "rgba(255, 0, 0, 0.3)"; // Semi-transparent red
+      ctx.fillRect(zone.x, zone.y, zone.width, zone.height);
+      ctx.strokeStyle = "red";
+      ctx.lineWidth = 2;
+      ctx.strokeRect(zone.x, zone.y, zone.width, zone.height);
+    });
 
     ctx.restore();
   };
